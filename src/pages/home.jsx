41db3d8684b33b700d20199c0a747939d53c78a4
Frosty273 <kelvinngor387@gmail.com>
2020-09-07 19:32:21 +1200

    Display data

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,32 +1,57 @@
 import React from 'react';
 import Search from '../components/search';
 import { fetchPokemon } from '../services/getPokemon';
+import PokemonData from '../components/pokemonData';
+import { Spinner, Alert } from 'react-bootstrap';
+
+const spinnerStyle = {
+    width: '10rem',
+    height: '10rem',
+    borderWidth: '1rem',
+}
+
+const spinnerWrapperStyle = {
+    textAlign: 'center',
+    marginTop: '50px',
+}
 
 export default function HomePage() {
 
     const [pokemon, setPokemon] = React.useState('');
     const [loading, setLoading] = React.useState(false);
+    const [error, setError] = React.useState(false);
 
     const getPokemon = async (query) => {
+        if (!query) {
+            return setError(true);
+        }
         setLoading(true);
-        const response = await fetchPokemon(query);
-        const results = await response.json();
-        console.log(results)
-        setPokemon(results);
-        setLoading(false);
+        setTimeout(async () => {
+            const response = await fetchPokemon(query);
+            const results = await response.json();
+            console.log(results)
+            setPokemon(results);
+            setLoading(false);
+            setError(false)
+        }, 1500);
     }
 
     return (
         <div>
-            <Search getPokemon={getPokemon}>
-                {!loading && pokemon ? (
-                    <div>
-                        <h1>
-                            {pokemon.name}
-                        </h1>
+            {error ? (
+                <Alert variant='danger'>
+                    You must enter a name.
+                </Alert>
+            ) : null}
+            <Search getPokemon={getPokemon}/>
+                {loading ? (
+                    <div style={spinnerWrapperStyle}>
+                        <Spinner style = {spinnerStyle} animation="border"/>
                     </div>
+                  )  : null}
+                {!loading && pokemon ? (
+                    <PokemonData name={pokemon.name} sprite={pokemon.sprites.front_default} abilities={pokemon.abilities} stats={pokemon.stats} types={pokemon.types}></PokemonData>
                 ) : null}
-            </Search>
         </div>
     )
 }
