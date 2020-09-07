04acae8ce70864b8228576e54ada567fabc66ec0
Frosty273 <kelvinngor387@gmail.com>
2020-09-07 17:08:57 +1200

    Add api

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Form, Container, Col} from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
+import { Form, Container, Col, Button} from 'react-bootstrap';
 
 export default function Search(props) {
 
+    const [search, setSearch] = React.useState('')
+
     return (
         <Container>
+            <h1>{search}</h1>
             <Form className="mt-2">
                 <Form.Row className="align-items-center">
                     <Col sm={10} className="my-1">
-                        <Form.Control placeholder="Search for Pokemon">
+                        <Form.Control placeholder="Search for Pokemon" onChange={(event) => setSearch(event.target.value)}>
 
                         </Form.Control>
                     </Col>
                     <Col sm={2} className="my-1">
-                        <Button block>Search</Button>
+                        <Button block onClick={(event) => props.getPokemon(search)}>Search</Button>
                     </Col>
                 </Form.Row>
             </Form>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import Search from '../components/search';
+import { fetchPokemon } from '../services/getPokemon';
 
 export default function HomePage() {
 
+    const [pokemon, setPokemon] = React.useState('');
+    const [loading, setLoading] = React.useState(false);
+
+    const getPokemon = async (query) => {
+        setLoading(true);
+        const response = await fetchPokemon(query);
+        const results = await response.json();
+        console.log(results)
+        setPokemon(results);
+        setLoading(false);
+    }
+
     return (
         <div>
-            <Search></Search>
+            <Search getPokemon={getPokemon}>
+                {!loading && pokemon ? (
+                    <div>
+                        <h1>
+                            {pokemon.name}
+                        </h1>
+                    </div>
+                ) : null}
+            </Search>
         </div>
     )
 }
