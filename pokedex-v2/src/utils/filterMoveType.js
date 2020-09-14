e870d8c8848a59600db8202613f86aae98a2db49
Frosty273 <kelvinngor387@gmail.com>
2020-09-14 17:40:13 +1200

    Refactor function

--- pokedex-v2/src/utils/filterMoveType.js
+++ pokedex-v2/src/utils/filterMoveType.js
@@ -1,12 +1,10 @@
 export function filterMoveType(move_name, version_group_detail_object, learn_method) {
-    let move_learn_level = {};
     const moves = version_group_detail_object.map((value) => (
-        move_learn_level[move_name] = value.level_learned_at,
         value.move_learn_method.name === learn_method && value.version_group.name === "ultra-sun-ultra-moon"? value.move_learn_method.name: null
     ));
     // console.log(version_group_detail_object)
     // console.log(move_learn_level)
     // console.log(moves)
-    return moves.includes(learn_method) ? (learn_method === "level-up" ? "Lv " + move_learn_level[move_name] + " " : "") + move_name : null
+    return moves.includes(learn_method) ? move_name : null
 
 }
